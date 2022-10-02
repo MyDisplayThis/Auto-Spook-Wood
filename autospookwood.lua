19d870051028cabe125fe75e16bbcad11469f6ee
Display <minecraftdafam@gmail.com>
2022-10-02 20:53:47 +0100

    Update autospookwood.lua

--- autospookwood.lua
+++ autospookwood.lua
@@ -1,3 +1,19 @@
+
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+
+local File = pcall(function()
+   AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+end)
+
+if not File then
+   table.insert(AllIDs, actualHour)
+   writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+end
+
 if game.PlaceId == 13822889 then
     
 if not game:IsLoaded() then game.Loaded:Wait() end
@@ -157,10 +173,67 @@ for i,v in pairs(workspace:GetChildren()) do
 	end
 end
 
+function TPReturner()
+   local Site;
+   if foundAnything == "" then
+       Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+   else
+       Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+   end
+   local ID = ""
+   if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+       foundAnything = Site.nextPageCursor
+   end
+   local num = 0;
+   for i,v in pairs(Site.data) do
+       local Possible = true
+       ID = tostring(v.id)
+       if tonumber(v.maxPlayers) > tonumber(v.playing) then
+           for _,Existing in pairs(AllIDs) do
+               if num ~= 0 then
+                   if ID == tostring(Existing) then
+                       Possible = false
+                   end
+               else
+                   if tonumber(actualHour) ~= tonumber(Existing) then
+                       local delFile = pcall(function()
+                           delfile("NotSameServers.json")
+                           AllIDs = {}
+                           table.insert(AllIDs, actualHour)
+                       end)
+                   end
+               end
+               num = num + 1
+           end
+           if Possible == true then
+               table.insert(AllIDs, ID)
+               wait()
+               pcall(function()
+                   writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                   wait()
+                   game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+               end)
+               wait(4)
+           end
+       end
+   end
+end
+
+function Teleport()
+   while wait() do
+       pcall(function()
+           TPReturner()
+           if foundAnything ~= "" then
+               TPReturner()
+           end
+       end)
+   end
+end
+
 if Tree then
     createGUI(tostring(Tree.TreeClass.Value).. " Wood Found", "New Server", "Teleport")
 Button1.MouseButton1Down:Connect(function()
-game:GetService("TeleportService"):Teleport(4019937000)
+Teleport()
 end)
 
 Button2.MouseButton1Down:Connect(function()
@@ -174,10 +247,10 @@ SpookWoodFinder:Destroy()
 end)
 
 Button2.MouseButton1Down:Connect(function()
-game:GetService("TeleportService"):Teleport(4019937000)
+Teleport()
 end)
 task.wait(delay)
-game:GetService("TeleportService"):Teleport(4019937000)
+Teleport()
 	
 end
 end
